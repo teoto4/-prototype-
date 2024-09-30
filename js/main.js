07577140132199a45ea97098d39e2b29fc11f2b1
teoto4 <teoto@ukr.net>
2024-10-01 01:34:24 +0300

    chrore(main.js) - add task 2

--- js/main.js
+++ js/main.js
@@ -5,62 +5,88 @@
 В імені та прізвищі можуть бути тільки латинські літери та тире.  
 Усі перевірки виконуються в сеттерах. */
 
-// const user = {
-//     name: "",
-//     lastName: "",
-//     number: "",
-//     email:"",
-//     set fullName(x){
-//         const nameReg = /^[A-Za-z\-]+$/;
-//         const [name, lastName] = x.split(" ");
-//         if (nameReg.test(name) && nameReg.test(lastName)) {
-//             this.name = name;
-//             this.lastName = lastName;
-//         } else {
-//             console.log("Use latin symbol");
-//         }
-//     },
-//     get fullName(){
-//         console.log(this.name, this.lastName);
-//         return`${this.name}, ${this.lastName}`
-//     },
-//     set phoneNumber(x){
-//         if('+380' === x.substr(0, 4)){
-//             console.log('true');
-//             this.number = x;
-//         } else {
-//             console.log('false');
-//             this.number = 'Wrong Number'  
-//         }
+const user = {
+    name: "",
+    lastName: "",
+    number: "",
+    email:"",
+    set fullName(x){
+        const nameReg = /^[A-Za-z\-]+$/;
+        const [name, lastName] = x.split(" ");
+        if (nameReg.test(name) && nameReg.test(lastName)) {
+            this.name = name;
+            this.lastName = lastName;
+        } else {
+            console.log("Use latin symbol");
+        }
+    },
+    get fullName(){
+        console.log(this.name, this.lastName);
+        return`${this.name}, ${this.lastName}`
+    },
+    set phoneNumber(x){
+        if('+380' === x.substr(0, 4)){
+            console.log('true');
+            this.number = x;
+        } else {
+            console.log('false');
+            this.number = 'Wrong Number'  
+        }
         
-//     },
-//     get phoneNumber(){
-//         console.log(this.number);
-//         return this.number;
-//     },
+    },
+    get phoneNumber(){
+        console.log(this.number);
+        return this.number;
+    },
 
-//     set emailAdd(x) {
-//         const mailReg = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
-//         if (mailReg.test(x)) {
-//             this.email = x;
-//         } else {
-//             console.log('Wrong email - set: gmail.com.');
-//         }
-//     },
+    set emailAdd(x) {
+        const mailReg = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+        if (mailReg.test(x)) {
+            this.email = x;
+        } else {
+            console.log('Wrong email - set: gmail.com.');
+        }
+    },
 
     
-//     get emailAdd() {
-//         return this.email;
-//     }
-// }
+    get emailAdd() {
+        return this.email;
+    }
+}
 
 
-// user.fullName = "Maks Lartsin";
-// user.fullName
-// user.phoneNumber = "+38093555555";
-// user.phoneNumber
-// user.emailAdd = "maks@gmail.com"
-// user.emailAdd
+user.fullName = "Maks Lartsin";
+user.fullName
+user.phoneNumber = "+38093555555";
+user.phoneNumber
+user.emailAdd = "maks@gmail.com"
+user.emailAdd
 
 
-/* */
+/*Завдання 2: Клас "Коло"
+Створіть клас `Circle`, який моделює коло. Клас має містити:
+1. Властивість `radius` для зберігання радіусу кола.
+2. Метод `getArea()`, який обчислює площу кола.
+3. Метод `getCircumference()`, який обчислює периметр кола.
+// Приклад використання
+const circle = new Circle(5);
+console.log("Площа:", circle.getArea()); // Площа: 78.54
+console.log("Периметр:", circle.getCircumference()); // Периметр: 31.42 */
+
+class Circle {
+    constructor(radius){
+        this.radius = radius;
+    }
+    getArea(){
+        const pi = 3.14159;
+        return pi * this.radius * this.radius; 
+    }
+    getPerimetr(){
+        const pi = 3.14159;
+        return 2 * pi * this.radius;
+    }
+}
+
+circle1 = new Circle(7);
+console.log(circle1.getArea());
+console.log(circle1.getPerimetr());
